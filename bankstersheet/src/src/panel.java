669c2f80c3ebf111498cd14f75990e2c26ca98ed
bernhard <bernhard@hergenroeder-online.de>
2019-06-06 01:31:27 +0200

    added to eclipse and produced a package

---
+++ bankstersheet/src/src/panel.java
@@ -0,0 +1,16 @@
+package src;
+
+import javax.swing.*;
+
+class Panel{
+    public static void main(String args[]){
+        JFrame frame = new JFrame("Bankstersheet");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        JPanel panel = new JPanel(true);
+        frame.getContentPane().add(panel);
+
+        frame.setVisible(true);
+    }
+}
